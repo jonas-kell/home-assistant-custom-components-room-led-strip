044d186702989b9b48b80c92421acecf666a9d1a
Jonas Kell <jonas-kell@web.de>
2023-07-14 11:37:43 +0200

    feat: store the state on th pico

--- pico-code/servercallback.py
+++ pico-code/servercallback.py
@@ -1,6 +1,7 @@
 from ledstrip import getLEDStrip
 from led import blink
 from server import parseUrl
+import json
 
 BLINK = False  # SET TO True FOR DEBUG
 
@@ -16,6 +17,9 @@ states = {
     # }
 }
 
+## INIT LIGHT STRIP
+" //TODO"
+
 
 def callback(method, url):
     global states
@@ -27,6 +31,19 @@ def callback(method, url):
     if method == "GET":
         if url == "/check_connect":
             return '{"status": "success"} \n'
+        if url == "/query":
+            try:
+                use_id = int(params["id"])
+            except Exception as ex:
+                print(
+                    f"ERROR: Parsing arguments in /init due to exception {type(ex).__name__}, {str(ex.args)}"
+                )
+                return '{"status": "error"}'
+            
+            state = get_state_and_assert_key_initialized(use_id)
+            print(state)
+
+            return f'{{"status": "success", "state": {state["state"]}, "brightness": {state["brightness"]}, "red": {state["red"]}, "green": {state["green"]}, "blue": {state["blue"]}}} \n'
 
     if method == "POST":
         if url == "/init":
@@ -41,7 +58,8 @@ def callback(method, url):
 
             new_id = len(states) + 1
 
-            states[new_id] = {
+            # init new
+            states[int(new_id)] = {
                 "index_from": index_from,
                 "index_to": index_to,
                 "state": False,
@@ -50,9 +68,62 @@ def callback(method, url):
                 "green": 0,
                 "blue": 0,
             }
-
+            # save to disk
+            save_state()
             print(states)
 
             return f'{{"status": "success", "id": {new_id}}} \n'
 
     return '{"status": "forbidden"} \n'
+
+
+def get_state_and_assert_key_initialized(get_id):
+    global states
+
+    get_id = int(get_id)
+
+    try:
+        state = states[get_id]
+    except:
+        print("state not present try loading from file:")
+        # try to load from disk
+        with open("store_state.json", "r") as f:
+            try:
+                data = json.load(f)
+                print("Loaded Data", data)
+
+                loaded_state = data[str(get_id)] # These functions always cast KEYS to str, bit sad, but hopefully consistent
+                print("Loaded State ", loaded_state)
+
+                states[get_id] = {}
+                states[get_id]["index_from"] = int(loaded_state["index_from"])
+                states[get_id]["index_to"] = int(loaded_state["index_to"])
+                states[get_id]["state"] = bool(loaded_state["state"])
+                states[get_id]["brightness"] = int(loaded_state["brightness"])
+                states[get_id]["red"] = int(loaded_state["red"])
+                states[get_id]["green"] = int(loaded_state["green"])
+                states[get_id]["blue"] = int(loaded_state["blue"])
+
+                print("State parsed from file:", states[get_id])
+            except Exception as ex:
+                print(f"Loading exception occured {type(ex).__name__}, {str(ex.args)}") # show exception without crashing
+
+                states[get_id] = {  # DEFAULT, should not be needed
+                    "index_from": 0,  # bad because not known
+                    "index_to": 10,  # bad because not known
+                    "state": False,
+                    "brightness": 0,
+                    "red": 0,
+                    "green": 0,
+                    "blue": 0,
+                }
+
+            f.close()
+
+    return states[get_id]
+
+
+def save_state():
+    with open("store_state.json", "w") as f:
+        json.dump(states, f)
+        f.close()
