37b46598c364e345d20d7740bf037375a07c6e5f
Jonas Kell <jonas-kell@web.de>
2023-07-14 10:49:59 +0200

    refactor: extract logic to own file

---
+++ pico-code/servercallback.py
@@ -0,0 +1,58 @@
+from ledstrip import getLEDStrip
+from led import blink
+from server import parseUrl
+
+BLINK = False  # SET TO True FOR DEBUG
+
+states = {
+    # 0: {
+    #     "index_from": 0,
+    #     "index_to": 0,
+    #     "state": False,
+    #     "brightness": 0,
+    #     "red": 0,
+    #     "green": 0,
+    #     "blue": 0,
+    # }
+}
+
+
+def callback(method, url):
+    global states
+
+    url, params = parseUrl(url)
+
+    blink(1, BLINK)  # show that communication happened
+
+    if method == "GET":
+        if url == "/check_connect":
+            return '{"status": "success"} \n'
+
+    if method == "POST":
+        if url == "/init":
+            try:
+                index_from = int(params["lif"])
+                index_to = int(params["lit"])
+            except Exception as ex:
+                print(
+                    f"ERROR: Parsing arguments in /init due to exception {type(ex).__name__}, {str(ex.args)}"
+                )
+                return '{"status": "error"}'
+
+            new_id = len(states) + 1
+
+            states[new_id] = {
+                "index_from": index_from,
+                "index_to": index_to,
+                "state": False,
+                "brightness": 0,
+                "red": 0,
+                "green": 0,
+                "blue": 0,
+            }
+
+            print(states)
+
+            return f'{{"status": "success", "id": {new_id}}} \n'
+
+    return '{"status": "forbidden"} \n'
